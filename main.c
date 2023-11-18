f71ed8788329a49006e11166c6a54cd55c7bbd2f
Matthew-M-dev <muyiwamatthew006@gmail.com>
2023-11-17 23:40:28 -0800

    all good

--- main.c
+++ main.c
@@ -22,24 +22,6 @@ int main(void)
 	printf("%d\n", INT_MIN);
 	_printf("Negative:[%d]\n", -762534);
 	printf("Negative:[%d]\n", -762534);
-	_printf("Unsigned:[%u]\n", ui);
-	printf("Unsigned:[%u]\n", ui);
-	_printf("Unsigned octal:[%o]\n", ui);
-	printf("Unsigned octal:[%o]\n", ui);
-	_printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-	printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-	_printf("Character:[%c]\n", 'H');
-	printf("Character:[%c]\n", 'H');
-	_printf("String:[%s]\n", "I am a string !");
-	printf("String:[%s]\n", "I am a string !");
-	_printf("Address:[%p]\n", addr);
-	printf("Address:[%p]\n", addr);
-	len = _printf("Percent:[%%]\n");
-	len2 = printf("Percent:[%%]\n");
-	_printf("Len:[%d]\n", len);
-	printf("Len:[%d]\n", len2);
-	_printf("Unknown:[%r]\n");
-	printf("Unknown:[%r]\n");
 	_printf("Let's print a simple sentence.\n");
 	printf("Let's print a simple sentence.\n");
 	_printf("%c\n", 'S');
@@ -59,9 +41,6 @@ int main(void)
 	_printf("%%");
 	printf("%%");
 	_printf("Should print a single percent sign: %%\n");
-	printf("Should print a single percent sign: %%\n");
-	_printf("%s%c%c%c%s%%%s%c", "Loading ", '.', '.', '.', " 99", " Please wait", '\n');
-	printf("%s%c%c%c%s%%%s%c", "Loading ", '.', '.', '.', " 99", " Please wait", '\n');
->>>>>>> d7716b238ec72d04f88880a0da861e61c9474245
+	_printf("Should print a single percent sign: %%\n");
 	return (0);
 }
